f474086398d3c716b73d29d641798b3ba9a7a074
Scott Fraser <scottfraserdev@gmail.com>
2018-10-22 17:10:42 -0700

    dealing with testing

---
+++ tests/Feature/SubmitLinksTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Validation\ValidationException;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SubmitLinksTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function guest_can_submit_a_new_link()
+    {
+        $response = $this->post('/submit', [
+            'title' => 'Example Title',
+            'url' => 'http://example.com',
+            'description' => 'Example description.',
+        ]);
+
+        $this->assertDatabaseHas('links', [
+            'title' => 'Example Title'
+        ]);
+
+        $response
+            ->assertStatus(302)
+            ->assertHeader('Location', url('/'));
+
+        $this
+            ->get('/')
+            ->assertSee('Example Title');
+    }
+
+
+    function link_is_not_created_if_validation_fails()
+    {
+        $response = $this->post('/submit');
+
+        $response->assertSessionHasErrors(['title', 'url', 'description']);
+    }
+    /** @test */
+    function link_is_not_created_with_an_invalid_url() {}
+
+    /** @test */
+    function max_length_fails_when_too_long() {}
+
+    /** @test */
+    function max_length_succeeds_when_under_max() {}
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
